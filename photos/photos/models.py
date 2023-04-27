a7e86d7ddbf22fd84b60efc17547b6d80adf0a44
David Bacisin <david@davidbacisin.com>
2023-04-26 20:43:35 -0500

    Add photo model

--- photos/photos/models.py
+++ photos/photos/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Photo(models.Model):
+    title = models.CharField(max_length=200)
+    image_url = models.CharField(max_length=200)
+    description = models.CharField(max_length=8000)
+    created_at = models.DateTimeField()
+    modified_at = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
